87932f0641be690748e705831119dfcdef48ba65
Abdallah Zahran <abdallahzahran29@gmail.com>
2026-06-16 14:59:56 +0300

    Implement Welcome Screen

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,66 +1,156 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:tasky/screens/home_screen.dart';
 
 class WelcomeScreen extends StatelessWidget {
-  const WelcomeScreen({super.key});
+  WelcomeScreen({super.key});
 
+  final TextEditingController nameController = TextEditingController();
+  final GlobalKey<FormState> _key = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Color(0xFF181818),
+    return Scaffold(
+      backgroundColor: const Color(0xFF181818),
       body: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            SizedBox(height: 16),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+        child: SingleChildScrollView(
+          child: Form(
+            key: _key,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                Image(image: AssetImage('assets/images/logo.png')),
-                SizedBox(width: 10),
-                Text(
-                  'Tasky',
+                const SizedBox(height: 16),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SvgPicture.asset(
+                      'assets/images/logo.svg',
+                      width: 24,
+                      height: 24,
+                    ),
+                    const SizedBox(width: 10),
+                    const Text(
+                      'Tasky',
+                      style: TextStyle(
+                        color: Color(0xFFFFFFFF),
+                        fontSize: 28,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 116),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      'Welcome To Tasky',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.w400,
+                        color: Color(0xFFFFFFFF),
+                      ),
+                    ),
+                    const SizedBox(width: 10),
+                    SvgPicture.asset(
+                      'assets/images/waving-hand-.svg',
+                      width: 24,
+                      height: 24,
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 8),
+                const Text(
+                  'Your productivity journey starts here.',
                   style: TextStyle(
-                    color: Color(0xFFFFFFFF),
-                    fontSize: 28,
+                    fontSize: 16,
                     fontWeight: FontWeight.w400,
+                    color: Color(0xFFFFFFFF),
+                  ),
+                ),
+                const SizedBox(height: 24),
+                SvgPicture.asset(
+                  'assets/images/welcome.svg',
+                  width: 215,
+                  height: 205,
+                ),
+                const SizedBox(height: 24),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        'Full Name',
+
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w400,
+                          color: Color(0xFFFFFFFF),
+                        ),
+                      ),
+                      const SizedBox(height: 8),
+                      TextFormField(
+                        style: const TextStyle(color: Colors.white),
+                        decoration: InputDecoration(
+                          hintText: 'Enter your name',
+                          hintStyle: const TextStyle(color: Color(0XFF6D6D6D)),
+                          filled: true,
+
+                          fillColor: const Color(0xFF282828),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(16),
+                            borderSide: BorderSide.none,
+                          ),
+                        ),
+                        cursorColor: Colors.white,
+                        controller: nameController,
+                        validator: (String? value) {
+                          if (value?.isEmpty ?? false) {
+                            return 'Please enter your name';
+                          }
+                        },
+                      ),
+                      const SizedBox(height: 24),
+                      ElevatedButton(
+                        onPressed: () {
+                          if (_key.currentState?.validate() ?? false) {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (BuildContext context) =>
+                                    const HomeScreen(),
+                              ),
+                            );
+                          } else {
+                            // show a snackbar
+                          }
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color(0xFF15B86C),
+                          foregroundColor: const Color(0xFFFFFCFC),
+                          fixedSize: Size(
+                            MediaQuery.of(context).size.width,
+                            40,
+                          ),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(16),
+                          ),
+                        ),
+                        child: const Text(
+                          "Let's Get Started",
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w400,
+                            color: Color(0xFFFFFFFF),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
               ],
             ),
-            SizedBox(height: 116),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'Welcome To Tasky',
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.w400,
-                    color: Color(0xFFFFFFFF),
-                  ),
-                ),
-                SizedBox(width: 10),
-                Image(
-                  image: AssetImage('assets/images/waving_hand.png'),
-                ),
-              ],
-            ),
-            SizedBox(height: 8),
-            Text(
-              'Your productivity journey starts here.',
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.w400,
-                color: Color(0xFFFFFFFF),
-              ),
-            ),
-            SizedBox(height: 24),
-            Image(
-              image: AssetImage('assets/images/pana.png'),
-              width: 215,
-              height: 205,
-            ),
-          ],
+          ),
         ),
       ),
     );
